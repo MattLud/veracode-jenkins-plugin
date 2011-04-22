baf00c68dcbfcdbbd393689f3f850661b83b95b3
John Madrid <john.madrid@M147AGY.rackspace.corp>
2011-04-22 15:47:59 -0500

    added some basic configuration grabbing to test it out

---
+++ src/main/java/com/rackspace/plugins/VeracodeNotifier.java
@@ -0,0 +1,11 @@
+package com.rackspace.plugins;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: john.madrid
+ * Date: 4/22/11
+ * Time: 11:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VeracodeNotifier {
+}
