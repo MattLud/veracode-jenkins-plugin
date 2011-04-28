24bf529093a2a23163895a91f7bddb72d02dd468
John Madrid <john.madrid@M147AGY.rackspace.corp>
2011-04-28 14:18:32 -0500

    commiting all changes before the 1.0 release

--- src/main/java/com/rackspace/plugins/VeracodeNotifier.java
+++ src/main/java/com/rackspace/plugins/VeracodeNotifier.java
@@ -1,21 +1,28 @@
 package com.rackspace.plugins;
 
+import com.rackspace.api.clients.veracode.DefaultVeracodeApiClient;
+import com.rackspace.api.clients.veracode.VeracodeApiClient;
+import com.rackspace.api.clients.veracode.VeracodeApiException;
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.remoting.VirtualChannel;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
 import net.sf.json.JSONObject;
-import org.jvnet.localizer.Localizable;
-import org.jvnet.localizer.ResourceBundleHolder;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,12 +34,18 @@ import java.io.IOException;
 public class VeracodeNotifier extends Notifier {
 
     private final String includes;
-    private final String excludes;
+    private final String username;
+    private final String password;
+    private final String applicationName;
+    private final int addToBuildNumber;
 
     @DataBoundConstructor
-    public VeracodeNotifier(String includes, String excludes) {
+    public VeracodeNotifier(String includes, String username, String password, String applicationName, String addToBuildNumber) {
         this.includes = includes;
-        this.excludes = excludes;
+        this.username = username;
+        this.password = password;
+        this.applicationName = applicationName;
+        this.addToBuildNumber = Integer.valueOf(addToBuildNumber);
     }
 
     @Override
@@ -46,9 +59,25 @@ public class VeracodeNotifier extends Notifier {
 
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
-        listener.getLogger().println("Includes: " + includes);
-        listener.getLogger().println("Excludes: " + excludes);
-        listener.getLogger().println("Endpoint: " + getDescriptor().getEndpoint());
+        FilePath workspace = build.getWorkspace();
+
+        FilePath[] filesToScan = workspace.list(includes);
+        listener.getLogger().println("Uploading Files to Veracode: " + Arrays.toString(filesToScan));
+        listener.getLogger().println("Base URI" + getDescriptor().getEndpoint());
+
+        VeracodeApiClient client = new DefaultVeracodeApiClient(getDescriptor().getEndpoint(), username, password, listener.getLogger());
+
+        String buildId = null;
+
+        try{
+            buildId = client.scanArtifacts(convertFilePaths(filesToScan), build.getNumber() + addToBuildNumber, applicationName);
+
+            listener.getLogger().println("Veracode Scan Succeeded. Build ID: " + buildId);
+        } catch(VeracodeApiException e){
+            throw new RuntimeException("Veracode Scan Failed", e);
+        } finally {
+            client.shutdown();
+        }
 
         return true;
     }
@@ -57,8 +86,32 @@ public class VeracodeNotifier extends Notifier {
         return includes;
     }
 
-    public String getExcludes() {
-        return excludes;
+    private List<File> convertFilePaths(FilePath[] filePaths){
+        List<File> files = new ArrayList<File>();
+
+        for(FilePath path : filePaths){
+            files.add(getFile(path));
+        }
+
+        return files;
+    }
+
+    private File getFile(FilePath path) {
+        File file;
+
+        try {
+            file = path.act(new FilePath.FileCallable<File>() {
+                public File invoke(File f, VirtualChannel channel) {
+                    return f;
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load the build artifact to send.", e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("The file operation was interrupted.", e);
+        }
+
+        return file;
     }
 
     @Override
@@ -73,6 +126,7 @@ public class VeracodeNotifier extends Notifier {
 
         public DescriptorImpl() {
             super(VeracodeNotifier.class);
+            endpoint = defaultEndpoint();
         }
 
         @Override
@@ -97,6 +151,10 @@ public class VeracodeNotifier extends Notifier {
 
         public String getEndpoint() {
             return endpoint;
+        }
+
+        public String defaultEndpoint(){
+            return "https://analysiscenter.veracode.com/api/";
         }
     }
 
